6a9f7be9cc8f54cadcae85f059473f6f1c67d41f
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-04-29 18:25:37 +0530

    ques2

---
+++ Exp7/q2/exp7.java
@@ -0,0 +1,17 @@
+package ishita;
+public class exp7
+{
+ public static void main(String[] args)
+ {
+  int n=0,m=10,r=0;
+  try
+  {
+   r=m/n;
+  }
+  catch(ArithmeticException e)
+  {
+   System.out.println("error");
+   
+  }
+}
+} 
