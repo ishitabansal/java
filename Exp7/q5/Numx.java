183567ea56a28d0dac1cd6ad7007811879678c61
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-04-29 18:27:09 +0530

    ques5

---
+++ Exp7/q5/Numx.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+class Numx
+{
+public static void main(String args[])
+ {
+String name;
+int age;
+System.out.println("-----ENTER EMPLOYEE DETAILS-----");
+System.out.println("Enter Name and Age:");
+Scanner in=new Scanner(System.in);
+
+try
+{
+if(!(in.nextLine().matches("[a-zA-Z]+")))
+{throw new IOException();}
+
+age=in.nextInt();
+if(age>50)
+{
+System.out.println("Age greater than 50 Exception");
+throw new Exception();
+}
+
+Numx x=new Numx();
+System.out.println("-----Object Created-----");
+}
+
+catch(Exception e)
+{
+System.out.println("Exception");
+}
+}
+}
