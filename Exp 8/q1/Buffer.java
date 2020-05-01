457ab4a5923124b64f5fafd4b5dac592c9999026
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-05-02 00:49:12 +0530

    ques1

---
+++ Exp 8/q1/Buffer.java
@@ -0,0 +1,24 @@
+public class Buffer
+{
+    public static String toUpperCase(StringBuffer s)
+{
+    String str = "";
+    for(int x=0; x<s.length(); x++){
+        char ch = s.charAt(x);
+        if(ch >= 'a' && ch <= 'z')
+            str += "" + (char)(ch - 32);
+        else
+            str += "" + ch; 
+    }
+    return str;
+}
+
+public static void main(String[] args)
+{
+    System.out.println(toUpperCase(new StringBuffer("ishita bansal")));
+        System.out.println(toUpperCase(new StringBuffer("rajat panwar")));
+      System.out.println(toUpperCase(new StringBuffer("nippun singhal")));
+      System.out.println(toUpperCase(new StringBuffer("mansi saini")));
+       System.out.println(toUpperCase(new StringBuffer("shivani chauhan")));  
+}
+}
