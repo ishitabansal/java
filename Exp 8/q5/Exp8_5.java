a035fccf6cd42a699fc78cdacd92804c7d19d952
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-05-02 00:52:36 +0530

    ques5

---
+++ Exp 8/q5/Exp8_5.java
@@ -0,0 +1,29 @@
+public class Exp8_5 {
+	 
+		  public static void main(String[] args) {
+		 
+		    Float Obj = new Float("22.23");
+		    
+		    byte b = Obj.byteValue();
+		    System.out.println(b);
+		   
+		    
+		    short s = Obj.shortValue();
+		    System.out.println(s);
+		   
+		    int i = Obj.intValue();
+		    System.out.println(i);
+		   
+		   
+		    float f = Obj.floatValue();
+		    System.out.println(f);
+		 
+		   
+		    double d = Obj.doubleValue();
+		    System.out.println(d);
+		    //float to double
+		    String si=Obj.toString();
+		    System.out.println(si);
+		 
+		  }
+		}
