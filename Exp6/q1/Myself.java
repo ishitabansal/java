64cd688a1370911d52b5735a211c403b26bf8d88
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-05-03 22:05:28 +0530

    first file

---
+++ Exp6/q1/Myself.java
@@ -0,0 +1,11 @@
+package Ishita;
+import java.util.Scanner;
+ public class Myself
+{          
+          
+     public void my_self()
+        {
+          System.out.println("I am ishita bansal and am pursuing my BTech fromUPES");
+        }
+  
+}
