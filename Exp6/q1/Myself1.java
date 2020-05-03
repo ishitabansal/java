487d668f19474c8dd6511cc703abea0e00d9ad9d
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-05-03 22:06:07 +0530

    file2

---
+++ Exp6/q1/Myself1.java
@@ -0,0 +1,9 @@
+import Ishita.Myself;                        
+public class Myself1
+{
+   public static void main(String[] args)
+     {
+        Myself obj=new Myself();
+         obj.my_self();
+       }
+}
