b6e9973ec1226901f1520faa26d123e75a06af9a
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-05-09 22:50:14 +0530

    ques2

---
+++ Exp9/q2/Exp9_2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+class Multi extends Thread
+{
+        public void run()
+        { 
+           for(int i=0;i<10;i++)
+              {
+                 if(i % 2 == 0)                           //check the no is even
+                  {
+                     System.out.println("even number: " +i);
+                   }
+               }
+          }
+}
+
+class Multi1 extends Thread
+{
+     public void run()
+     {
+        for(int i=0;i<10;i++)
+          {
+              if(i % 2 == 1)                            //check the no is odd
+                {
+                  System.out.println("odd number: " +i);
+                 }
+          }
+      }
+}
+
+public class Exp9_2
+{
+    public static void main(String[] args)
+       {
+            Multi t=new Multi();
+            t.start();
+            Multi1 t1=new Multi1();
+             t1.start();
+         }
+}
