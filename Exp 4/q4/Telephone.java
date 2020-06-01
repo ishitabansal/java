56d7c2c42c09c373bb91212774c39e6a4e7d795e
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-01 23:26:28 +0530

    Telephone

--- Exp 4/q4/Telephone.java
+++ Exp 4/q4/Telephone.java
@@ -1,58 +1,58 @@
 import java.util.*;
 class Call
 {
-float dur;
-String type;
-float rate()
-{
-if(type.equals("urgent"))
-return 4.5f;
-else if(type=="lightning")
-return 3.5f;
-else
-return 3f;
-}
+  float dur;
+  String type;
+  float rate()
+  {
+    if(type.equals("urgent"))
+      return 4.5f;
+    else if(type=="lightning")
+      return 3.5f;
+    else
+      return 3f;
+  }
 }
 class Bill extends Call
 {
-float amount;
-void read()
-{
-Scanner input=new Scanner(System.in);
-System.out.println("enter call type(urgent,lihtning,ordinary):");
-type=input.next();
-System.out.println("enter call duration");
-dur=input.nextFloat();
-}
-void Calculate()
-{
-if(dur<=1.5)
-amount=rate()*dur+1.5f;
-else if(dur<=3)
-amount=rate()*dur+2.5f;
-else if(dur<=5)
-amount=rate()*dur+4.5f;
-else
-amount=rate()*dur+5f;
+  float amount;
+  void read()
+  {
+   Scanner input=new Scanner(System.in);
+   System.out.println("enter call type(urgent,lihtning,ordinary):");
+   type=input.next();
+   System.out.println("enter call duration");
+   dur=input.nextFloat();
+  }
+  void Calculate()
+  {
+  if(dur<=1.5)
+    amount=rate()*dur+1.5f;
+  else if(dur<=3)
+    amount=rate()*dur+2.5f;
+  else if(dur<=5)
+    amount=rate()*dur+4.5f;
+  else
+    amount=rate()*dur+5f;
 }
 void print()
 {
-System.out.println("************");
-System.out.println("PHONE BILL");
-System.out.println("**************");
-System.out.println("call type:"+type);
-System.out.println("duration:"+dur);
-System.out.println("charge:"+amount);
-System.out.println("***************");
+   System.out.println("************");
+   System.out.println("PHONE BILL");
+   System.out.println("**************");
+   System.out.println("call type:"+type);
+   System.out.println("duration:"+dur);
+   System.out.println("charge:"+amount);
+   System.out.println("***************");
 }
 }
 class Telephone
 {
-public static void main(String args[])
-{
-Bill b=new Bill();
-b.read();
-b.Calculate();
-b.print();
-}
+   public static void main(String args[])
+  {
+   Bill b=new Bill();
+   b.read();
+   b.Calculate();
+   b.print();
+  }
 }     
