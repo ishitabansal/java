bc1dee01c1537841152bfbb18015ff6a4acb2f42
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-01 23:15:28 +0530

    jwellery_box

--- Exp 4/q1/Jwellery_box.java
+++ Exp 4/q1/Jwellery_box.java
@@ -1,34 +1,34 @@
 class Box
 {
-private int l,b;
-Box(int x,int y)
-{l=x; b=y;};
-int area()
-{
- return(l*b);
-}
+  private int l,b;
+  Box(int x,int y)
+  {l=x; b=y;};
+  int area()
+  {
+   return(l*b);
+  }
 }
 class Rectangle extends Box
 {
- int h;
- Rectangle(int x,int y,int z)
- {
-   super(x,y);
-   h=z;
- }
- int volume()
- {
- return(area()*h);
- }
+   int h;
+   Rectangle(int x,int y,int z)
+   {
+     super(x,y);
+     h=z;
+   }
+   int volume()
+   {
+     return(area()*h);
+   }
 }
- public class Jwellery_box
+public class Jwellery_box
 { 
- public static void main(String[] args)
- {
-  Rectangle rc=new Rectangle(10,20,15);
-  int a1=rc.area();
-  int v1=rc.volume();
-  System.out.println("Area of room:"+a1);
-  System.out.println("Volume of room:"+v1);
- }
+  public static void main(String[] args)
+  {
+   Rectangle rc=new Rectangle(10,20,15);
+   int a1=rc.area();
+   int v1=rc.volume();
+   System.out.println("Area of room:"+a1);
+   System.out.println("Volume of room:"+v1);
+  }
 }
