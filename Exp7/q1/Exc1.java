0ca5b68618d57cd3657d9e6d9d42c1570342a862
ishitabansal <46719880+ishitabansal@users.noreply.github.com>
2020-06-03 23:04:20 +0530

    Exc1

--- Exp7/q1/Exc1.java
+++ Exp7/q1/Exc1.java
@@ -4,7 +4,8 @@ import java.util.*;
   
            public static void main(String[] args)
              { 
-                for(int i=0;i<10;i++) {
+                for(int i=0;i<10;i++) 
+                {
               Scanner sc=new Scanner(System.in);
   
               System.out.println("enter the name : ");
